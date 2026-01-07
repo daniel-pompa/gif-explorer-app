fe7a11b5daa08429b6372011fdb6f08a009a9b54
Daniel <daniel.40.pompa@gmail.com>
2026-01-08 00:00:22 +0100

    feat(app): integrate search logic and state management

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,29 +1,42 @@
-import { mockGifs } from './mock-data/gifs.mock';
+import type { Gif } from './gifs/interfaces/gif.interface';
 import { Header, SearchBar } from './shared/components';
 import { GifList, SearchHistory } from './gifs/components';
+import { useState } from 'react';
+import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action';
+
 export const GifsApp = () => {
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const [recentSearches, setRecentSearches] = useState<string[]>([]);
+
+  const handleSearch = async (query: string = '') => {
+    query = query.trim().toLowerCase();
+
+    if (query.length === 0) return;
+
+    if (recentSearches.includes(query)) return;
+
+    setRecentSearches([query, ...recentSearches].splice(0, 6));
+
+    const gifs = await getGifsByQuery(query);
+
+    setGifs(gifs);
+  };
+
+  const handleTermClicked = (searchTerm: string) => {
+    console.log({ searchTerm });
+  };
+
   return (
     <>
       <Header
         title='Gifs Explorer'
         description='Discover the best animated GIFs from GIPHY.'
       />
-      <SearchBar placeholder='Search GIFs' />
-      <SearchHistory
-        searches={[
-          'Dragon Ball',
-          'Goku',
-          'Jubei Kibagami',
-          'Hommer Simpson',
-          'Bart Simpson',
-          'Iron Man',
-          'Funny cat',
-          'Baby dancing',
-        ]}
-      />
+      <SearchBar placeholder='Search GIFs' onQuery={handleSearch} />
+      <SearchHistory searches={recentSearches} onSelectTerm={handleTermClicked} />
 
       {/* Gifs */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   );
 };
