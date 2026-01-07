1cf50447c032cbea2acfdbcd6309d9a24e330bbb
Daniel <daniel.40.pompa@gmail.com>
2026-01-07 23:58:33 +0100

    feat(api): implement giphy service and search action

---
+++ src/gifs/actions/get-gifs-by-query.action.ts
@@ -0,0 +1,20 @@
+import { giphyAPI } from '../api/giphy.api';
+import type { GiphyResponse } from '../interfaces/giphy.response';
+import type { Gif } from '../interfaces/gif.interface';
+
+export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
+  const response = await giphyAPI.get<GiphyResponse>('/search', {
+    params: {
+      q: query,
+      limit: 20,
+    },
+  });
+
+  return response.data.data.map(gif => ({
+    id: gif.id,
+    title: gif.title,
+    url: gif.images.original.url,
+    width: Number(gif.images.original.width),
+    height: Number(gif.images.original.height),
+  }));
+};
