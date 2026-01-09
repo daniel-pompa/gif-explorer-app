59295e4f8a29bbf791b832c59c908d48931c9d01
Daniel <daniel.40.pompa@gmail.com>
2026-01-09 17:12:56 +0100

    test(config): exclude non-critical files from coverage

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    coverage: {
+      exclude: ['src/utils/logger.ts', 'src/main.tsx', '**/interfaces/**'],
+    },
   },
 });
